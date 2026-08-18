d5d6659b9c7fdde8d293422493aa05d1d44d1d6e
Anubhav Kiroula <anubhavkiroula1@gmail.com>
2026-08-18 14:51:02 +0530

    Time: 0 ms (100%), Space: 9.7 MB (18.46%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+        set<int> st;
+        for(int i=0;i<s.size();++i){
+            if(isdigit(s[i])){
+                st.insert(s[i]-'0');
+            }
+        }
+        if(st.size() < 2) return -1;
+        vector<int> ans(st.begin(),st.end());
+        return ans[ans.size() -2];
+    }
+};
