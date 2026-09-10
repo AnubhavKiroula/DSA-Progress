a8b994e362a4c8b34297c20aaa2db350f4cbb29b
Anubhav Kiroula <anubhavkiroula1@gmail.com>
2026-09-10 20:51:25 +0530

    Time: 0 ms (100%), Space: 8 MB (12.66%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n>0 && 1073741824%n == 0) return true;
+        if(n<=0) return false;
+        while(n % 2 == 0) n = n/2;
+        if(n == 1) return true;
         return false;
     }
 };
